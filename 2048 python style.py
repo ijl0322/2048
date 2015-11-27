c7fd3716b99ec9e10e6731d408747e653b33c2d8
Isabel Lee <Issy@IsabeldeMBP.home>
2015-11-26 23:03:29 -0800

    pushDown function added
    add pushDown function

--- 2048 python style.py
+++ 2048 python style.py
@@ -32,52 +32,79 @@ def checklose(board):
                 totalNum += 1
     return totalNum < 16 
 
-#def pushUP(UserInput):
-#    if UserInput.lower() == "u":
-#        for i in range(4):
-#            if board[0][i] == "0""
-#                board[0][i] =  
 
-def pushUP():
-    UserInput = raw_input("Enter:")
-    if UserInput == "u":
+
+def pushUp():
                 
-        for i in range(3,0,-1):
-            for j in range(4):
-                #check if 2 numbers are the same, if yes, add them together
-                if board[i][j] == board[i-1][j]:
-                    board[i-1][j] = str(int(board[i][j])+int(board[i-1][j]))
-                    board[i][j] = "0"
-                #check if the slot above is empty, if so, push up the number below
-                if board[i-1][j] == "0":
-                    board[i-1][j] = board[i][j]
-                    board[i][j] = "0"
+    for i in range(3,0,-1):
+        for j in range(4):
+            #check if 2 numbers are the same, if yes, add them together
+            if board[i][j] == board[i-1][j]:
+                board[i-1][j] = str(int(board[i][j])+int(board[i-1][j]))
+                board[i][j] = "0"
+            #check if the slot above is empty, if so, push up the number below
+            if board[i-1][j] == "0":
+                board[i-1][j] = board[i][j]
+                board[i][j] = "0"
+            
+    #make sure every number is correctly pushed up
+    for i in range(3):
+        for j in range(4):             
+            if board[i][j] == "0" and board[i-1][j] == "0":
+                board[i][j] = board[i+1][j]
+                board[i+1][j] = "0"
+                board[i-1][j] = board[i][j]
+                board[i][j] = "0"
+            elif board[i][j] == "0":
+                board[i][j] = board[i+1][j]
+                board[i+1][j] = "0"
+
+def pushDown():
                 
-        #make sure every number is correctly pushed up
-        #showboard(board)
-        for i in range(3):
-            for j in range(4):             
-                if board[i][j] == "0":
-                    board[i][j] = board[i+1][j]
-                    board[i+1][j] = "0"
-                    if board[i-1][j] == "0":
-                        board[i-1][j] = board[i][j]
-                        board[i][j] = "0"
-                
-                
+    for i in range(3):
+        for j in range(4):
+            #check if 2 numbers are the same, if yes, add them together
+            if board[i][j] == board[i+1][j]:
+                board[i+1][j] = str(int(board[i][j])+int(board[i+1][j]))
+                board[i][j] = "0"
+            #check if the slot above is empty, if so, push up the number below
+            if board[i+1][j] == "0":
+                board[i+1][j] = board[i][j]
+                board[i][j] = "0"
+            
+    #make sure every number is correctly pushed up
+    for i in range(3,1,-1):
+        for j in range(4):             
+            if board[i][j] == "0" and board[i-1][j] == "0":
+                board[i][j] = board[i+1][j]
+                board[i+1][j] = "0"
+                board[i-1][j] = board[i][j]
+                board[i][j] = "0"
+            elif board[i][j] == "0":
+                board[i][j] = board[i-1][j]
+                board[i-1][j] = "0"
+
+
+def main():
+    
+    print "Hi, welcome to 2048 game!"
+    print "Enter u to move all numbers upward"
+    print "Enter d to move all numbers downward"
+    print "Enter l to move all numbers tp the left"
+    print "Enter r to move all numbers to the right"
+    print " "
+    
     showboard(board)
-
-    
-    
+    UserInput = raw_input("Enter u, d, l or r:")
+    if UserInput == "u":    
+        pushUp()
+    elif UserInput == "d":    
+        pushDown()
+    showboard(board)
   
-#board[3][0] = "5"
-#board[2][0] = "5"    
-#board[3][1] = "3"
-#board[2][1] = "3" 
+
 
 for i in range(10):
     addNewNum()
-showboard(board)  
-pushUP()
-#showboard(board)  
-print checklose(board)
+  
+main()
