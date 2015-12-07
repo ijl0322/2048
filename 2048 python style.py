9800bdc30a5db665d2202c4e6924dbbdde3e6152
Isabel Lee <ijl0322@gmail.com>
2015-12-07 15:47:11 +0800

    Edited Docstrings

--- 2048 python style.py
+++ 2048 python style.py
@@ -20,9 +20,8 @@ def showboard(board):
                 print "    ", item,
         print "\n\n"
 
-
 def addNewNum():
-    """Generates a 2 or 4, and set at a random location on board\
+    """Generates a 2 or 4, and set the number at a random location on board \
     that is originally a 0 """
     
     newNum = str(random.choice([2,4]))
@@ -34,17 +33,22 @@ def addNewNum():
     board[randomy][randomx] = newNum 
         
 def checklose(board):
-    """Takes in a list (board), return True if there is still 0 in the list, otherwise False"""
+    """Takes in a list (board), return True the player does not lose (if there is still 0 in the list or \
+    if any move can still be made) otherwise False"""
     totalNum = 0
     for line in board:
         for num in line:
             if num != "0":
                 totalNum += 1
-    
-    return totalNum < 16                
+    if totalNum == 16:
+        return pushDirection("u") + pushDirection("d") + pushDirection("l") + pushDirection("r") != 0
+    return True            
                
     
 def checkwin(board):
+    """ Checks if the player has the number 2048 on the board. 
+    If so, return True and print a "you win" statement. Otherwise, return False."""
+    
     flag = False
     for line in board:
         for num in line:
@@ -54,6 +58,15 @@ def checkwin(board):
     return flag
                 
 def add(i_list, j_list, i_direction, j_direction):
+    
+    """Iterates through the board, and adds a number with its adjacent neighbor if the two numbers are the same. 
+    ex. 2248 becomes 0448.
+    i_list, j_list - lists, indicate how add interate through the list.
+    i_direction, j_direction - an int, either 1, -1 or 0. Defines the direction of the add. 
+    ex. +1, 2248 becomes 0448. -1, 2248 becomes 4480, 0, does not add. 
+    
+    move is a counter that's later used to determine whether a move can still be made by the player.
+    """
     move = 0
     for i in i_list:
         for j in j_list:
@@ -68,6 +81,13 @@ def add(i_list, j_list, i_direction, j_direction):
     return move
     
 def push(i_list, j_list, i_direction, j_direction):
+    
+    """push a number to its adjacent slot if the slot is 0. 
+    i_list, j_list - lists, indicate how push interate through the list.
+    i_direction, j_direction - an int, either 1, -1 or 0. Defines the direction of the push. 
+    move is a counter that's later used to determine whether a move can still be made by the player.
+    """
+   
     move = 0
     for i in i_list:
         for j in j_list:
@@ -79,6 +99,12 @@ def push(i_list, j_list, i_direction, j_direction):
     return move                
 
 def pushDirection(UserInput):
+    """
+    Takes in a UserInput and calls add and push function with the proper i_list,\
+    j_list, i_direction and j_direction. 
+    UserInput - a str
+    """
+    
     move = 0
     if UserInput == "u":
         i_list, j_list = range(1,4), range(4)
@@ -93,8 +119,6 @@ def pushDirection(UserInput):
         i_list, j_list = range(4), range(2,-1,-1)
         i_direction, j_direction = 0, 1
        
-    #for i in range(4): 
-
     for i in range(4):
         move += push(i_list, j_list, i_direction, j_direction)
     move += add(i_list, j_list, i_direction, j_direction)
@@ -113,27 +137,33 @@ def main():
     print " "
     addNewNum() 
     addNewNum()     
-    showboard(board)                      
+    showboard(board)   
+                                            
     while checklose(board):
-                                         
+        
         if checkwin(board):
-            print "You win"
             break
-        elif not checklose(board):
-            print "Sorry, Game over"
-            
+                                                     
         UserInput = raw_input("Enter u, d, l or r:")
         print "\n"
-        if UserInput in "udlr":
-            move = pushDirection(UserInput) 
-            if move != 0:
-                addNewNum()                   
-        else:
-            print "Invalid input, please try again."  
+        try:
+            if UserInput in "udlr":
+                move = pushDirection(UserInput) 
+                if move != 0:
+                    addNewNum()                   
+            else:
+                print "Invalid input, please try again."  
+        except:
+            print "Invalid input, please try again."
+            continue
         
         showboard(board) 
         
+    if not checklose(board):
+        print "Sorry, Game over"
+
 #debugging code
+
 """
 board[0][1] = "4"
 board[0][2] = "2"
@@ -148,8 +178,8 @@ board[2][2] = "16"
 board[2][3] = "128"
 board[3][0] = "4"
 board[3][1] = "2"
-board[3][2] = "8"
-board[3][3] = "256"   
+board[3][2] = "1024"
+board[3][3] = "1024"   
 """
 
 if __name__=='__main__':
